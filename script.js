ae9e4e56f962491436eb545bb13a42bfdceb499c
Nargroth108 <138591543+Nargroth108@users.noreply.github.com>
2023-12-13 18:10:59 +0100

    Write an Operate function
    This calls one of the 4 basic functions based n the operator.

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ let firstNumber;
 let operator;
 let secondNumber;
 
+function operate(operator, firstNumber, secondNumber) {
+    if(operator === '+') {
+        firstNumber = add(firstNumber, secondNumber);
+        return firstNumber
+    } else if(operator === '-') {
+        firstNumber = subtract(firstNumber, secondNumber);
+        return firstNumber
+    } else if(operator === '*') {
+        firstNumber = multiply(firstNumber, secondNumber);
+        return firstNumber
+    } else if(operator === '/') {
+        firstNumber = divide(firstNumber, secondNumber);
+        return firstNumber
+    }
+}
+
+console.log(operate("/", 12, 6))
