0a1a98b90d771d28e59c6e5e41a7c8da9eb982af
Nargroth108 <138591543+Nargroth108@users.noreply.github.com>
2023-12-14 12:17:32 +0100

    Add functions that populate the display with the values of the buttons when those are pressed

--- script.js
+++ script.js
@@ -3,24 +3,69 @@ subtract = (a, b) => a - b;
 multiply = (a, b) => a * b;
 divide = (a, b) => a / b;
 
-let firstNumber;
-let operator;
-let secondNumber;
+let display = '';
+
+let firstNumber = null;
+let operator = null;
+let secondNumber = null;
+let numNext = true;
 
 function operate(operator, firstNumber, secondNumber) {
-    if(operator === '+') {
+    if(operator === ' + ') {
         firstNumber = add(firstNumber, secondNumber);
         return firstNumber
-    } else if(operator === '-') {
+    } else if(operator === ' - ') {
         firstNumber = subtract(firstNumber, secondNumber);
         return firstNumber
-    } else if(operator === '*') {
+    } else if(operator === ' * ') {
         firstNumber = multiply(firstNumber, secondNumber);
         return firstNumber
-    } else if(operator === '/') {
+    } else if(operator === ' / ') {
         firstNumber = divide(firstNumber, secondNumber);
         return firstNumber
     }
-}
+};
 
-console.log(operate("/", 12, 6))
+const numBtn = document.querySelectorAll('.number');
+numBtn.forEach(function(button) {
+    button.addEventListener('click', () => {
+        let text = button.textContent;
+        display += text;
+        document.querySelector('#display').textContent = display;
+        if (numNext === true) {
+            numNext = false;
+            opBtn.forEach((button) => {
+                button.disabled = false;
+            });
+        };
+    });
+});
+
+const opBtn = document.querySelectorAll('.operation');
+opBtn.forEach(function(button) {
+    button.addEventListener('click', () => {
+        let text = button.textContent;
+        display += text;
+        document.querySelector('#display').textContent = display;
+        numNext = true;
+        opBtn.forEach((button) => {
+            button.disabled = true;
+        });
+    });
+});
+
+const clearBtn = document.querySelector('.C');
+clearBtn.addEventListener('click', () => {
+    display = '';
+    firstNumber = secondNumber = operator = null;
+    document.querySelector('#display').textContent = display;
+    numNext = true;
+    opBtn.forEach((button) => {
+        button.disabled = false;
+    });
+});
+
+const equalBtn = document.querySelector('.E');
+equalBtn.addEventListener('click', () => {
+
+})
